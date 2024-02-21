cd1e62434e4ff611b39d2f736c5bfebc3d0af003
Your Name <you@example.com>
2024-02-21 18:48:55 +0800

    fix

--- upx.sh
+++ upx.sh
@@ -4,6 +4,7 @@
 # see also https://github.com/rust-lang/cargo/issues/545
 
 tmp=$(mktemp || exit)
+trap "rm $tmp" EXIT
 
 tee $tmp <<"EOF"
 if type upx
@@ -11,19 +12,25 @@ then
 	upx $1 && exit
 fi
 
-exit
+if test "$(cat $1|wc -c)" -lt 31457280
+then
+	echo skipping file size less than 30M
+	exit
+fi
 
 if type xz
 then
-	cat $1 | xz -c -v -e -9 > $1.xz && exit
+	cat $1 | xz -c -v -e -9 > $1.xz && mv $1.xz $1 && exit
 fi
 
 if type gzip
 then
-	cat $1 | gzip -c -9 > $1.gz && exit
+	cat $1 | gzip -c -9 > $1.gz && mv $1.gz $1 && exit
 fi
 
-echo failed to compress file size, fallback to strip?
+echo failed to compress file size, fallback to strip.
+
+strip $1
 EOF
 
 command $*
