d97b1dce8d60c3e73e6a39d1d981b2b29fa9229b
Your Name <you@example.com>
2024-02-21 15:53:38 +0800

    CI build

---
+++ upx.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# a warpper for "post-run" build scripts
+# see also https://github.com/rust-lang/cargo/issues/545
+
+tmp=$(mktemp || exit)
+
+tee $tmp <<"EOF"
+if type upx
+then
+	upx $1 && exit
+fi
+
+exit
+
+if type xz
+then
+	cat $1 | xz -c -v -e -9 > $1.xz && exit
+fi
+
+if type gzip
+then
+	cat $1 | gzip -c -9 > $1.gz && exit
+fi
+
+echo failed to compress file size, fallback to strip?
+EOF
+
+command $*
+status_code="$?"
+find ./target/ \( -name geph4-exit -or -name geph4-exit.exe \) -exec sh $tmp '{}' \;
+exit "$status_code"
+
